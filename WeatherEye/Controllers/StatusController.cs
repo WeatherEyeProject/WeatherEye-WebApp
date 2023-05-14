bb39cd07cbd57a73bf398adfd46b5e2aa6e13cf4
kadziewiczjakub <259099@student.pwr.edu.pl>
2023-05-14 20:46:36 +0200

    api status + https enforcment disabled for now

---
+++ WeatherEye/Controllers/StatusController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherEye.Controllers
+{
+    [Route("api/status")]
+    [ApiController]
+    public class StatusController : Controller
+    {
+        [HttpGet]
+        public IActionResult getStatus()
+        {
+            return Ok("OK");
+        }
+    }
+}
