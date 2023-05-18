cca2792a52b9d1cd01172e1098877d68fd6f9522
kadziewiczjakub <259099@student.pwr.edu.pl>
2023-05-18 20:04:12 +0200

    Post Api Auth Test
    Post Api Auth Test

--- WeatherEye/Controllers/SensorsDataGatherer.cs
+++ WeatherEye/Controllers/SensorsDataGatherer.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json.Serialization; 
 using WeatherEye.Interfaces;
 using WeatherEye.Models;
+using System.Text.Json;
 
 namespace WeatherEye.Controllers
 {
@@ -21,6 +25,51 @@ namespace WeatherEye.Controllers
         {
             var res = await _sensorsDataGatherer.AddDataAsync(sensorsDataList);
             return res ? Ok(sensorsDataList) : BadRequest(sensorsDataList);
+        }
+
+        [HttpPost("/test")]
+        public async Task<IActionResult> PostSensorData2([FromBody] string data, [FromHeader] string authorization)
+        {
+            if(!isAuthValid(data, authorization))
+            {
+                return Unauthorized();
+            }
+            List<SensorsData> models = JsonSerializer.Deserialize<List<SensorsData>>(data);
+            var res = await _sensorsDataGatherer.AddDataAsync(models);
+            return res ? Ok(models) : BadRequest();
+        }
+
+        private bool isAuthValid(string data, string auth)
+        {
+            string[] headerVals = auth.Split(':');
+            if(headerVals.Length != 2 || headerVals[0] != "HMAC")
+            {
+                return false;
+            }
+            return headerVals[1] == calcHmac(data);
+        }
+
+        private string calcHmac(string data)
+        {
+            byte[] secretBytes = readFile("key.txt"); //Encoding.UTF8.GetBytes(secretKey);
+            byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+            using (HMACSHA256 hmac = new HMACSHA256(secretBytes))
+            {
+                byte[] hmacBytes = hmac.ComputeHash(dataBytes);
+                return Convert.ToBase64String(hmacBytes);
+            }
+        }
+
+        private byte[] readFile(string path)
+        {
+            byte[] res = null;
+            using(StreamReader sr =  new StreamReader(path)) 
+            {
+                var strBytes = sr.ReadToEnd();
+                var splits = strBytes.Split(',');
+                res = splits.Select(s => Convert.ToByte(s, 16)).ToArray();
+            }
+            return res;
         }
     }
 }
